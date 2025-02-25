50c900606fab3d32720c9eccb1069858a5bfda4e
Stanislav Kozubenko <staspk@gmail.com>
2025-02-24 21:05:13 -0800

    Optimization Done

--- Kozubenko/IO/File.cs
+++ Kozubenko/IO/File.cs
@@ -10,45 +10,52 @@ namespace Kozubenko.IO
     public class File
     {
         /// <summary>
-        /// On Windows, creates, if path does not exist, and returns:                C:\Users\{user}\AppData\Roaming\{ApplicationName}\{configFileName}
-        /// 
-        /// On Unix/Linux, creates, if path does not exist, and returns:            /home/{username}/{ApplicationName}/{configFileName}
-        /// 
-        /// On exception, will fallback to, create if does not exist, and returns:  {path/to/assembly}/{configFileName}
+        ///     On Windows, creates, if path does not exist, and returns:                 C:\Users\{user}\AppData\Roaming\{ApplicationName}\{configFileName}
+        ///     On Unix / Linux, creates, if path does not exist, and returns:            home/{user}/{ApplicationName}/{configFileName}
+        ///     On Exception, will fallback to, create if does not exist, and returns:    {path/to/executable}/{configFileName}
         /// </summary>
         /// <param name="ApplicationName"></param>
         /// <param name="configFileName"></param>
         /// <returns></returns>
-        public static string GenerateConfigFile(string ApplicationName, string configFileName = "log.txt")
+        public static string GenerateConfigFilePath(string ApplicationName, string configFileName = "db.sqlite3")
         {
             try
             {
                 string roaming = OperatingSystem.IsWindows() ? Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
                                                              : Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 
-                string roamingAppDataDirectory = Path.Combine(roaming, ApplicationName);
+                string appDataDirectory = Path.Combine(roaming, ApplicationName);
 
-                if (!Directory.Exists(roamingAppDataDirectory))
-                    Directory.CreateDirectory(roamingAppDataDirectory);
+                if (!Directory.Exists(appDataDirectory))
+                    Directory.CreateDirectory(appDataDirectory);
 
-                string roamingConfigFile = Path.Combine(roaming, ApplicationName, configFileName);
-
-                if (!System.IO.File.Exists(roamingConfigFile))
-                    System.IO.File.Create(roamingConfigFile);
-
-                return roamingConfigFile;
+                return Path.Combine(appDataDirectory, configFileName);
             }
             catch
             {
                 string executableDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).ToString();
 
-                string fallbackConfigFile = Path.Combine(executableDirectory, configFileName);
+                string fallbackDataDirectory = Path.Combine(executableDirectory, ".app_data");
 
-                if(!System.IO.File.Exists(fallbackConfigFile))
-                    System.IO.File.Create(fallbackConfigFile);
+                if (!Directory.Exists(fallbackDataDirectory))
+                    Directory.CreateDirectory(fallbackDataDirectory);
 
-                return fallbackConfigFile;
+                return Path.Combine(fallbackDataDirectory, configFileName);
             }
+        }
+
+        /// <summary>
+        ///     Behavior identical to GenerateConfigFile, but does not create configFile if it does not exist.
+        ///     It's parent directory will still be created, if it does not exist.
+        /// </summary>
+        public static string GenerateConfigFile(string ApplicationName, string configFileName = "log.txt")
+        {
+            string appDataDirectory = GenerateConfigFilePath(ApplicationName, configFileName);
+
+            if (!System.IO.File.Exists(appDataDirectory))
+                System.IO.File.Create(appDataDirectory);
+
+            return appDataDirectory;
         }
     }
 }
